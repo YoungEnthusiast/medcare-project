80aecb43d828db7efdcaf85403b68d6b47e19c4e
Yus <yustaoab@gmail.com>
2021-07-07 02:58:30 +0100

    up

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -101,6 +101,10 @@ DATABASES = {
     }
 }
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
