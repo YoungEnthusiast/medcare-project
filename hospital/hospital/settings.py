086d0abae528ac8f2d8551b13a265727d33e78cb
Yus <yustaoab@gmail.com>
2021-08-02 12:04:58 +0100

    up

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -89,12 +89,7 @@ MESSAGE_TAGS = {
 # EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.zoho.com'
-DEFAULT_FROM_EMAIL = 'Med Care Hospitals <info@medcarehospitals.com.ng>'
-EMAIL_HOST_USER = 'info@medcarehospitals.com.ng'
-EMAIL_HOST_PASSWORD = 'Medcare987654321!'
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
