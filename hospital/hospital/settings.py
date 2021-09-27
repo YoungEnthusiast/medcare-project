31f468663e33384a66c8f5eb762730038f37f755
Yus <yustaoab@gmail.com>
2021-09-27 14:49:26 +0100

    up

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -95,8 +95,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'yustaoab@gmail.com'
 EMAIL_HOST_PASSWORD = 'twnqanloovwgjrsq'
 EMAIL_USE_SSL = True
-EMAIL_PORT = 587
-
+#EMAIL_PORT = 587
+EMAIL_PORT = 465
 
 ## EMAIL_HOST_USER = 'info@medcarehospitals.com.ng'
 # EMAIL_HOST_PASSWORD = 'Medcare987654321!'
